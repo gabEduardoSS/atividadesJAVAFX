f3da2adb6d250ee3684fc0e2a3e94e9da3d64445
eduardo <eduardo90508036@edu.unipar.br>
2025-11-02 19:56:21 -0300

    at3

--- atividade2/src/main/java/edu/atts/atividade2/temp.java
+++ atividade2/src/main/java/edu/atts/atividade2/temp.java
@@ -3,12 +3,58 @@ package edu.atts.atividade2;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.scene.input.KeyEvent;
+import org.w3c.dom.Text;
 
 public class temp {
     @FXML
-    private TextField cels;
+    private TextField celsius;
     @FXML
-    private  TextField fahr;
+    private TextField fahr;
+    @FXML
+    private Label aviso;
+    @FXML
+    private Label resultado;
 
+    @FXML
+    private void blockTF(KeyEvent event) {
+        Object origem = event.getSource();
+        String digitado = event.getCharacter();
+        String charTemp = "";
+        String tf = "";
+
+        if (origem == celsius) {
+            fahr.setDisable(true);
+            charTemp = "C";
+            tf = celsius.getText();
+            if(celsius.getText().isEmpty()){
+                fahr.setDisable(false);
+            }
+        } else if (origem == fahr) {
+            celsius.setDisable(true);
+            charTemp = "F";
+            tf = fahr.getText();
+            if(fahr.getText().isEmpty()){
+                celsius.setDisable(false);
+            }
+        }
+
+        if (!digitado.matches("[0-9]|.")){
+            event.consume();
+        } else if(!charTemp.isEmpty() && !tf.isEmpty()){
+            converter(tf, charTemp);
+        } else{
+            resultado.setText("Cº = Fº");
+        }
+    }
+
+    private void converter(String tf, String charTemp){
+        double temp = Double.parseDouble(tf);
+        String PH = "%.2f Cº = %.2f Fº";
+        if(charTemp.equals("C")){
+            resultado.setText(String.format(PH, temp, (temp*9/5)+32));
+        } else if(charTemp.equals("F")){
+            resultado.setText(String.format(PH, (temp-32)*5/9, temp));
+        }
+    }
 }
--- atividade2/src/main/java/module-info.java
+++ atividade2/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module edu.atts.atividade2 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.xml;
 
     opens edu.atts.atividade2 to javafx.fxml;
     exports edu.atts.atividade2;

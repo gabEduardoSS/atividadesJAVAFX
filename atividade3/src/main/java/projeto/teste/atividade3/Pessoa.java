7ba2e478a0b6b3941f174d67a38f0a8603f73223
eduardo <eduardo90508036@edu.unipar.br>
2025-11-09 13:55:56 -0300

    att4

--- atividade3/src/main/java/projeto/teste/atividade3/Pessoa.java
+++ atividade3/src/main/java/projeto/teste/atividade3/Pessoa.java
@@ -9,16 +9,52 @@ import javafx.scene.text.Text;
 
 public class Pessoa {
     @FXML
-    private TextField nome;
+    private TextField nomeTF;
     @FXML
-    private TextField idade;
+    private TextField idadeTF;
     @FXML
-    private ComboBox<String> sexo;
+    private ComboBox<String> sexoCB;
     @FXML
-    private CheckBox esporte;
+    private CheckBox esporteCB;
 
     @FXML
-    protected void analise(){
+    private Label nomeLB;
+    @FXML
+    private Label idadeLB;
+    @FXML
+    private Label sexoLB;
+    @FXML
+    private Label esporteLB;
+    @FXML
+    private Label maiorLB;
+    @FXML
+    private Label avisoLB;
 
+    @FXML
+    public void analise(){
+        String nome =  nomeTF.getText();
+        String idade = idadeTF.getText();
+        String sexo = sexoCB.getSelectionModel().getSelectedItem();
+        String esporte = esporteCB.isSelected() ? "Pratica" : "Não pratica";
+        avisoLB.setText("");
+
+
+        if(!nome.isEmpty() && !idade.isEmpty()){
+            if(idade.matches("[0-9]*")){
+                if(!sexo.equalsIgnoreCase("sexo")){
+                    nomeLB.setText(String.format("%s", nome));
+                    idadeLB.setText(String.format("%s", idade));
+                    sexoLB.setText(String.format("%s", sexo));
+                    esporteLB.setText(String.format("%s esportes", esporte));
+                    maiorLB.setText(Integer.parseInt(idade) >= 18 ? "Sim" : "Não");
+                } else {
+                    avisoLB.setText("Insira um sexo");
+                }
+            } else {
+                avisoLB.setText("O campo idade aceita apenas números");
+            }
+        } else {
+            avisoLB.setText("Preencha todos os campos");
+        }
     }
 }
